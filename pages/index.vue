685314367edf12448fc3950de1549ce595748d76
DanielJin0715 <danieljin0715@gmail.com>
2024-02-08 10:55:41 -0800

    Poll posts from the API each 30 seconds

--- pages/index.vue
+++ pages/index.vue
@@ -10,12 +10,37 @@ const favorite = useFavorite()
 
 await post.fetchPosts()
 await favorite.fetchFavorites()
+
+const disabled = ref(false)
+
+setInterval(async () => {
+  const data = await $api.get('/posts')
+  if (data.data.length > post.posts.length) {
+    disabled.value = false
+  }
+}, 30000, this)
+
+async function load () {
+  await post.fetchPosts();
+  disabled.value = true;
+}
+
 </script>
 
 <template>
   <PostForm
     v-if="!user.isGuest" />
   <div class="grid gap-16">
+    <button  
+        class="font-medium text-md px-2 rounded-full"
+        :class="{
+          'bg-blue-200': !disabled,
+          'bg-gray-200': disabled
+        }"
+        :disabled="disabled"
+        @click="load">
+        Load New Posts
+      </button>
     <PostItem
       v-for="post in post.posts"
       :key="post.id"
